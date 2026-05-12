3fd03af0a33b68db8c07d436ecf61c064327d3f0
rutujajawaldapke-svg <rutujajawaldapke@gmail.com>
2026-05-12 23:28:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.getElementById("status");
+const restartBtn = document.getElementById("restartBtn");
+
+const popup = document.getElementById("popup");
+const popupMessage = document.getElementById("popupMessage");
+
+let currentPlayer = "X";
+let gameActive = true;
+
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningConditions = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+];
+
+statusText.textContent = `Player ${currentPlayer}'s Turn`;
+
+cells.forEach(cell => {
+  cell.addEventListener("click", handleCellClick);
+});
+
+restartBtn.addEventListener("click", restartGame);
+
+function handleCellClick(){
+
+  const cellIndex = this.getAttribute("data-index");
+
+  if(gameState[cellIndex] !== "" || !gameActive){
+    return;
+  }
+
+  gameState[cellIndex] = currentPlayer;
+
+  this.textContent = currentPlayer;
+
+  checkWinner();
+}
+
+function checkWinner(){
+
+  let roundWon = false;
+
+  for(let i = 0; i < winningConditions.length; i++){
+
+    const condition = winningConditions[i];
+
+    const a = gameState[condition[0]];
+    const b = gameState[condition[1]];
+    const c = gameState[condition[2]];
+
+    if(a === "" || b === "" || c === ""){
+      continue;
+    }
+
+    if(a === b && b === c){
+      roundWon = true;
+      break;
+    }
+  }
+
+  if(roundWon){
+
+    popup.style.display = "flex";
+
+    popupMessage.textContent =
+      `Player ${currentPlayer} Wins!`;
+
+    gameActive = false;
+
+    return;
+  }
+
+  if(!gameState.includes("")){
+
+    popup.style.display = "flex";
+
+    popupMessage.textContent =
+      "Game Draw!";
+
+    gameActive = false;
+
+    return;
+  }
+
+  currentPlayer =
+    currentPlayer === "X" ? "O" : "X";
+
+  statusText.textContent =
+    `Player ${currentPlayer}'s Turn`;
+}
+
+function restartGame(){
+
+  currentPlayer = "X";
+
+  gameActive = true;
+
+  gameState =
+    ["", "", "", "", "", "", "", "", ""];
+
+  statusText.textContent =
+    `Player ${currentPlayer}'s Turn`;
+
+  cells.forEach(cell => {
+    cell.textContent = "";
+  });
+
+  popup.style.display = "none";
+}
